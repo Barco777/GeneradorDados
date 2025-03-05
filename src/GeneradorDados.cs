f821b1a67a832f45ed1fecfd1e21d4d804533d92
Barco777 <a6908@palmore.edu.mx>
2025-03-04 20:01:14 -0600

    Organizada estructura del proyecto y movido GeneradorDados.cs a src

---
+++ src/GeneradorDados.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GeneradorDados
+{
+    public class Dados
+    {
+        private static readonly Random random = new Random();
+
+        public static int LanzarDado(int caras)
+        {
+            if (caras != 6 && caras != 10 && caras != 20)
+                throw new ArgumentException("Solo se permiten dados de 6, 10 o 20 caras.");
+
+            return random.Next(1, caras + 1);
+        }
+    }
+}
