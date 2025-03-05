3bb1038e22ebb99ded436db040042ec7dfb74896
Barco777 <124919568+Barco777@users.noreply.github.com>
2025-03-04 20:11:31 -0600

    GeneradorDados.cs

--- src/GeneradorDados.cs
+++ src/GeneradorDados.cs
@@ -13,11 +13,6 @@ namespace GeneradorDados
 
             return random.Next(1, caras + 1);
         }
-
-        public static int LanzarDado12()
-        {
-            return random.Next(1, 13);
-        }
     }
 }
 
